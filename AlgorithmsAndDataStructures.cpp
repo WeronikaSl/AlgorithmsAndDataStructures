e12eb7932c406505830cdc2cb1a391bb3f664c21
WeronikaSl <105433644+WeronikaSl@users.noreply.github.com>
2024-01-11 19:12:55 +0100

    Implement dynamic version of stack

--- AlgorithmsAndDataStructures.cpp
+++ AlgorithmsAndDataStructures.cpp
@@ -2,10 +2,11 @@
 #include <string>
 #include "LinkedList.hpp"
 #include "StackStatic.hpp"
+#include "StackDynamic.hpp"
 
 int main()
 {
-    //LINKED LIST
+    ////LINKED LIST
 
     //LinkedList<uint16_t> linkedListWithInts;
 
@@ -38,23 +39,47 @@ int main()
     //linkedListWithStrings.pushFront(":D");
     //linkedListWithStrings.displayElements();
 
-    //STACK STATIC
-    StackStatic<uint16_t> stackStaticWithInt;
-    stackStaticWithInt.pop();
-    std::cout << stackStaticWithInt.size() << std::endl;
-    stackStaticWithInt.push(6);
-    std::cout << stackStaticWithInt.top() << std::endl;    
-    std::cout << stackStaticWithInt.size() << std::endl;
-    stackStaticWithInt.push(10);
-    std::cout << stackStaticWithInt.top() << std::endl;
-    std::cout << stackStaticWithInt.size() << std::endl;
+    ////STACK STATIC
+    //StackStatic<uint16_t> stackStaticWithInt;
+    //std::cout << stackStaticWithInt.top() << std::endl;
+    //stackStaticWithInt.pop();
+    //std::cout << stackStaticWithInt.size() << std::endl;
+    //stackStaticWithInt.push(6);
+    //std::cout << stackStaticWithInt.top() << std::endl;    
+    //std::cout << stackStaticWithInt.size() << std::endl;
+    //stackStaticWithInt.push(10);
+    //std::cout << stackStaticWithInt.top() << std::endl;
+    //std::cout << stackStaticWithInt.size() << std::endl;
 
-    StackStatic<std::string> stackStaticWithString;
-    stackStaticWithString.push("Hi");
-    std::cout << stackStaticWithString.top() << std::endl;
-    stackStaticWithString.pop();
-    stackStaticWithString.pop();
+    //StackStatic<std::string> stackStaticWithString;
+    //stackStaticWithString.push("Hi");
+    //std::cout << stackStaticWithString.top() << std::endl;
+    //stackStaticWithString.pop();
+    //stackStaticWithString.pop();
+    //std::cout << stackStaticWithString.top() << std::endl;
 
+    ////STACK DYNAMIC
+    StackDynamic<uint16_t> stackDynamicWithInt;
+    std::cout << stackDynamicWithInt.peek() << std::endl;
+    std::cout << stackDynamicWithInt.isEmpty() << std::endl;
+    stackDynamicWithInt.pop();
+    stackDynamicWithInt.push(10);
+    std::cout << stackDynamicWithInt.peek() << std::endl;
+    stackDynamicWithInt.push(12);
+    std::cout << stackDynamicWithInt.peek() << std::endl;
+    stackDynamicWithInt.pop();
+    std::cout << stackDynamicWithInt.peek() << std::endl;
+    std::cout << stackDynamicWithInt.isEmpty() << std::endl;
 
-
+    StackDynamic<std::string> stackDynamicWithString;
+    std::cout << stackDynamicWithString.peek() << std::endl;
+    std::cout << stackDynamicWithString.isEmpty() << std::endl;
+    stackDynamicWithString.pop();
+    stackDynamicWithString.push("ok");
+    std::cout << stackDynamicWithString.peek() << std::endl;
+    stackDynamicWithString.push("oke");
+    std::cout << stackDynamicWithString.peek() << std::endl;
+    stackDynamicWithString.pop();
+    std::cout << stackDynamicWithString.peek() << std::endl;
+    std::cout << stackDynamicWithString.isEmpty() << std::endl;
 }
---
+++ StackDynamic.hpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "Node.hpp"
+
+//Avaliable methods:
+//void pop();
+//void push(T);
+//T peek() const;
+//bool isEmpty() const;
+
+template <typename T> class StackDynamic
+{
+private:
+	Node<T>* top{ nullptr };
+public:
+	void pop()
+	{
+		if (top == nullptr)
+		{
+			std::cout << "Stack underflow" << std::endl;
+		}
+		else
+		{
+			Node<T>* newTop{ top->locationOfNextNode };
+			delete top;
+			top = newTop;
+		}
+	}
+	void push(T value)
+	{
+		if (top == nullptr)
+		{
+			top = new Node<T>(value, nullptr);
+		}
+		else
+		{
+			Node<T>* newTop = new Node<T>(value, top);
+			top = newTop;
+		}
+	}
+	T peek() const
+	{
+		if (top == nullptr)
+		{
+			return T{}; //is using rvalue like this correct?
+		}
+		else
+		{
+			return top->valueOfThisNode;
+		}
+	}
+	bool isEmpty() const
+	{
+		if (top == nullptr)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	~StackDynamic()
+	{
+		if (top != nullptr)
+		{
+			Node<T>* currentNode{ top };
+			Node<T>* nodeToDelete{ nullptr };
+			while (currentNode != nullptr)
+			{
+				nodeToDelete = currentNode;
+				currentNode = currentNode->locationOfNextNode;
+				delete nodeToDelete;
+			}
+			delete currentNode;
+			top = nullptr;
+			nodeToDelete = nullptr;
+			currentNode = nullptr;
+		}
+	}
+};
